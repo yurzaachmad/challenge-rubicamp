cfd68e3ca59f2225696009878d1024296421cdf6
yurza <yurzaachmad@gmail.com>
2023-05-16 22:28:42 +0700

    challenge-4

---
+++ challenge4.js
@@ -0,0 +1,25 @@
+function indexPrime(param1){
+  let index = 0;
+  let count = 2
+ 
+  while (index < param1){
+    let isPrime = true;
+  for (let number = 2; number < count; number++) {
+    if(count % number === 0){
+      isPrime = false;
+    }
+      
+  }
+  if(isPrime == true){
+    index++
+  } 
+    count++
+  
+}
+return count -1
+}
+
+console.log(indexPrime(10))
+console.log(indexPrime(25))
+console.log(indexPrime(13))
+console.log(indexPrime(12))
