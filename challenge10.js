29d9e342b143949c7fd4d7b4bdf238ee0fb688c4
yurza <yurzaachmad@gmail.com>
2023-05-20 22:05:44 +0700

    readline node.js

--- challenge10.js
+++ challenge10.js
@@ -1,13 +1,3 @@
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-
-//   readline.question('Tulis kalimatmu disini >', name => {
-//     console.log(`Hey there ${name}!`);
-//     readline.close();
-//   });
-  
 const readline = require('node:readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -27,7 +17,6 @@ rl.on('line', (line) => {
 });
 
 function sentencesManipulation(sentence) {
-    // my arr sekumpulan kalimat yang jadi array
 
     let myArr = sentence.split(" ");
     let arr = []
@@ -36,14 +25,10 @@ function sentencesManipulation(sentence) {
     for (let i = 0; i < myArr.length; i++) {
         if (myArr[i].charAt(0) === 'a' || myArr[i].charAt(0) === 'u' || myArr[i].charAt(0) === 'i' || myArr[i].charAt(0) === 'e' || myArr[i].charAt(0) === 'o') {
            arr.push(myArr[i])
-        //    console.log(myArr);
         } else {
             
            let myConsonant = myArr[i].slice(1)+ myArr[i].charAt(0) + 'nyo';
             
-           
-        //    myConsonant.slice(1)
-        //    console.log(myConsonant);
             arr.push(myConsonant) 
         }
        
