dc81c1a2c8c904698fa3e670ab900f9c991bd00f
yurza <yurzaachmad@gmail.com>
2023-05-25 19:59:14 +0700

    erd and sql database

---
+++ challenge14/challenge14.js
@@ -0,0 +1,6 @@
+sqlite3 university.db
+create table dosen(nip varchar(3) primary key not null, name varchar(100) not null, alamat text);
+create table students(nim varchar(3) primary key not null, nama varchar(100) not null, alamat text not null, idJurusan varchar(3) not null, foreign key(idJurusan) references jurusan(idJurusan));
+create table jurusan(idJurusan varchar(3) primary key not null, namaJurusan varchar(100) not null);
+create table mataKuliah(idMataKuliah varchar(3) primary key not null, namaMataKuliah varchar(100) not null, sks INTEGER(20));
+   
