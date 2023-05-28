2be6a15f829949ac2985fc7cdf1712212c8162c9
yurza <yurzaachmad@gmail.com>
2023-05-28 19:58:51 +0700

    function string manipulation

--- challenge5.js
+++ challenge5.js
@@ -1,17 +1,30 @@
+//function string manipulation
+
 function stringManipulation(word) {
-    if(word[0] === 'a' || word[0] === 'i' || word[0] === 'u' || word[0] === 'e' || word[0] === 'o'){
-        return word
-        
-    } else {
-        const chars = word.split("")
-        if(chars[0] != 'a' || chars[0] != 'i' || chars[0] != 'u' || chars[0] != 'e' || chars[0] != 'o'){
-            chars.push(chars[0]);
-            chars.shift();
-            let sheaf = chars.join('') 
-            return sheaf + 'nyo'
-        }
+  if (
+    word[0] === "a" ||
+    word[0] === "i" ||
+    word[0] === "u" ||
+    word[0] === "e" ||
+    word[0] === "o"
+  ) {
+    return word;
+  } else {
+    const chars = word.split("");
+    if (
+      chars[0] != "a" ||
+      chars[0] != "i" ||
+      chars[0] != "u" ||
+      chars[0] != "e" ||
+      chars[0] != "o"
+    ) {
+      chars.push(chars[0]);
+      chars.shift();
+      let sheaf = chars.join("");
+      return sheaf + "nyo";
     }
+  }
 }
 
-console.log(stringManipulation('ayam'))
-console.log(stringManipulation('bebek'))
+console.log(stringManipulation("ayam"));
+console.log(stringManipulation("bebek"));
