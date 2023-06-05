b0bd16af9f095b3f1652d29cba7cc4844f663615
yurza <yurzaachmad@gmail.com>
2023-06-05 20:02:41 +0700

    strings manipulation

--- challenge6.js
+++ challenge6.js
@@ -1,29 +1,28 @@
+//manipulation strings
+
 function sentencesManipulation(sentence) {
-    // my arr sekumpulan kalimat yang jadi array
+  // my arr sekumpulan kalimat yang jadi array
 
-    let myArr = sentence.split(" ");
-    let arr = []
-    
-    
-    for (let i = 0; i < myArr.length; i++) {
-        if (myArr[i].charAt(0) === 'a' || myArr[i].charAt(0) === 'u' || myArr[i].charAt(0) === 'i' || myArr[i].charAt(0) === 'e' || myArr[i].charAt(0) === 'o') {
-           arr.push(myArr[i])
-        //    console.log(myArr);
-        } else {
-            
-           let myConsonant = myArr[i].slice(1)+ myArr[i].charAt(0) + 'nyo';
-            
-           
-        //    myConsonant.slice(1)
-        //    console.log(myConsonant);
-            arr.push(myConsonant) 
-        }
-       
+  let myArr = sentence.split(" ");
+  let arr = [];
+
+  for (let i = 0; i < myArr.length; i++) {
+    if (
+      myArr[i].charAt(0) === "a" ||
+      myArr[i].charAt(0) === "u" ||
+      myArr[i].charAt(0) === "i" ||
+      myArr[i].charAt(0) === "e" ||
+      myArr[i].charAt(0) === "o"
+    ) {
+      arr.push(myArr[i]);
+      //    console.log(myArr);
+    } else {
+      let myConsonant = myArr[i].slice(1) + myArr[i].charAt(0) + "nyo";
+      arr.push(myConsonant);
     }
+  }
 
-    return arr.join(" ")
+  return arr.join(" ");
+}
 
-    }
-
-
-console.log(sentencesManipulation('ibu pergi ke pasar bersama aku'))
+console.log(sentencesManipulation("ibu pergi ke pasar bersama aku"));
